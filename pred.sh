eabec380a1a37b06b13af6155088eae4b387dba5
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-02-21 15:24:33 +0900

    add second_kits result

--- pred.sh
+++ pred.sh
@@ -2,8 +2,8 @@
 YAML=$1
 GPU=$2
 
-python3 ./src/set_hdf5.py -yml $YAML 
-python3 ./src/Keras/pred3D_wrap.py -yml $YAML -g $GPU
-python3 ./src/evaluate/preprocess_evaluate.py -yml $YAML
+# python3 ./src/set_hdf5.py -yml $YAML 
+# python3 ./src/Keras/pred3D_wrap.py -yml $YAML -g $GPU
+python3 ./src/evaluate/preprocess_evaluate.py -exp_yml $YAML
 python3 ./src/evaluate/evaluate_tsukuba_med.py -yml $YAML
 python3 ./src/evaluate/evaluate_plot.py -yml $YAML
